e08747f63b7b8a76609c620cb83ef710b8b26ede
pulum03 <pulum03@gmail.com>
2019-12-16 16:30:06 +0900

    added easy sql questions

---
+++ SQL/WeatherObservationStation5.sql
@@ -0,0 +1,21 @@
+/*
+Enter your query here.
+*/
+-- SELECT city, LENGTH(city)
+-- FROM station
+-- ORDER BY length(city) limit 1;
+-- SELECT city, LENGTH(city)
+-- FROM station
+-- ORDER BY length(city) desc limit 1
+
+SELECT CITY,LENGTH(CITY) 
+FROM STATION 
+WHERE LENGTH(CITY) = (
+    SELECT MIN(LENGTH(CITY)) FROM STATION)   
+ORDER BY CITY LIMIT 1;
+
+SELECT CITY,LENGTH(CITY) 
+FROM STATION 
+WHERE LENGTH(CITY) = (
+    SELECT MAX(LENGTH(CITY)) FROM STATION) 
+ORDER BY CITY LIMIT 1;
