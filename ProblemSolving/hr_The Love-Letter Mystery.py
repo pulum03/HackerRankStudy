6b152791c3900cb8c71b9453f16263aa277124a5
pulum03 <pulum03@gmail.com>
2020-01-22 23:38:32 +0900

    2020.1.22

---
+++ ProblemSolving/hr_The Love-Letter Mystery.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the theLoveLetterMystery function below.
+def theLoveLetterMystery(s):
+    res = 0
+    
+    for i in range(len(s) // 2):
+        res += abs(ord(s[i]) - ord(s[len(s) - i - 1]))
+    
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
