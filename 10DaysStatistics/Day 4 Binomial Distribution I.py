139c45adc71b8f7674a121457763cc36479516a2
pulum03 <pulum03@gmail.com>
2020-02-06 17:53:09 +0900

    2020.2.6

---
+++ 10DaysStatistics/Day 4 Binomial Distribution I.py
@@ -0,0 +1,18 @@
+from math import factorial
+
+def bionomial(x,n,p):
+    factional_part = factorial(n)/(factorial(x)*factorial(n-x))
+    result =factional_part*p**x*(1-p)**(n-x)
+    return result
+
+
+if __name__ == "__main__":
+    a,b = map(float, input().split())
+    p = a/(a+b)
+    n = 6
+    answer = 0
+    
+    for x in range(3,7):
+        answer+= bionomial(x,n,p)
+
+    print(round(answer,3))
