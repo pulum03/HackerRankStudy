626e7a6dc16fca1a0158d440e4f902f22d3abf4f
pulum03 <pulum03@gmail.com>
2020-02-17 16:32:09 +0900

    2020.2.17

---
+++ ProblemSolving/hr_Running Time of Algorithms.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the runningTime function below.
+def runningTime(arr):
+    
+    cnt = 0
+    
+    if arr==sorted(arr):
+        return (0)    
+    else:
+        for i in range(1,len(arr)):
+            x=arr[i]
+
+            for j in range(i):
+                if j>=0 and arr[j]>x:
+                    (arr[i],arr[j])=(arr[j],arr[i])
+                    cnt += 1
+        
+    return cnt
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = runningTime(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
